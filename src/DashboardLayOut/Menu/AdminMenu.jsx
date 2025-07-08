02f49f518b71057f4f48103b670602a9028b6841
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-07-08 18:38:58 +0600

    Admin section Menu added done

--- src/DashboardLayOut/Menu/AdminMenu.jsx
+++ src/DashboardLayOut/Menu/AdminMenu.jsx
@@ -1,11 +1,15 @@
 import { FaUserCog } from 'react-icons/fa'
+import { RiAdminFill } from "react-icons/ri";
+import { FaUsersCog } from "react-icons/fa";
 import MenuItem from './MenuItem'
-import { BsGraphUp } from 'react-icons/bs'
+import { FaStarHalfAlt } from "react-icons/fa";
 const AdminMenu = () => {
   return (
     <>
-      <MenuItem icon={BsGraphUp} label='Statistics' address='/dashboard' />
-      <MenuItem icon={FaUserCog} label='Manage Users' address='manage-users' />
+      <MenuItem icon={RiAdminFill} label='Admin Profile' address='adminProfile' />
+      <MenuItem icon={FaUserCog} label='Manage Properties' address='manageProperties' />
+      <MenuItem icon={FaUsersCog } label='Manage Users' address='manageUsers' />
+      <MenuItem icon={FaStarHalfAlt} label='Manage reviews' address='manageReviews' />
     </>
   )
 }
--- src/DashboardLayOut/Sidebar/Sidebar.jsx
+++ src/DashboardLayOut/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-// import { GrLogout } from 'react-icons/gr'
+import { GrLogout } from 'react-icons/gr'
 // import { FcSettings } from 'react-icons/fc'
 import { AiOutlineBars } from 'react-icons/ai'
 // import { BsGraphUp } from 'react-icons/bs'
@@ -62,7 +62,7 @@ const Sidebar = () => {
       >
         <div>
           <div>
-            <div className='w-full hidden md:flex  shadow-lg rounded-lg justify-center items-center bg-gray-200 mx-auto'>
+            <div className='w-full  shadow-lg rounded-lg justify-center items-center bg-gray-200 mx-auto'>
               <Link className='flex items-center font-extrabold' to='/'>
                 <img
                   // className='hidden md:block'
@@ -105,7 +105,7 @@ const Sidebar = () => {
             onClick={logOut}
             className='flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-gray-300   hover:text-gray-700 transition-colors duration-300 transform'
           >
-            {/* <GrLogout className='w-5 h-5' /> */}
+            <GrLogout className='w-5 h-5' />
 
             <span className='mx-4 font-medium'>Logout</span>
           </button>
