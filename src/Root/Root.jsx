5ba0a6acfed70b8c783291207b6c386046037371
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-07-08 09:26:13 +0600

    all package install done

---
+++ src/Root/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../Components/NavBar/NavBar';
+import { Outlet } from 'react-router';
+import Footer from '../Components/Footer/Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <NavBar/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Root;
